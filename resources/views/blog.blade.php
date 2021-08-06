21f284aa97cdad6b8e438b52d41bb86688d24b05
Shubham Jain <jshubham579@gmail.com>
2021-08-06 17:52:25 +0530

    blog page created

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="css/blog.css">
+</head>
+
+@include('navbar')
+
+<main>
+    <div class="blog">
+        <img src="images/jackson-hayes-JdlIfpb9dgM-unsplash.jpg" alt="">
+        <content>
+            <h1>Blog Title</h1>
+            <h3>Author : Shubham Jain</h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam in nobis, quas aspernatur doloremque
+                dolorum enim fugit, numquam quia assumenda asperiores sed, laboriosam fugiat repudiandae laborum
+                debitis.
+                Distinctio, aperiam eos!</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora blanditiis molestiae possimus obcaecati
+                deserunt, unde nihil pariatur praesentium asperiores doloribus vel et dolorum ab doloremque eius
+                similique
+                incidunt nisi quos.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium nulla optio dolores, laudantium
+                fugiat
+                similique velit facere nesciunt unde sapiente consequatur id soluta quibusdam praesentium cupiditate
+                dicta
+                quidem facilis adipisci?</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam in nobis, quas aspernatur doloremque
+                dolorum enim fugit, numquam quia assumenda asperiores sed, laboriosam fugiat repudiandae laborum
+                debitis.
+                Distinctio, aperiam eos!</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora blanditiis molestiae possimus obcaecati
+                deserunt, unde nihil pariatur praesentium asperiores doloribus vel et dolorum ab doloremque eius
+                similique
+                incidunt nisi quos.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium nulla optio dolores, laudantium
+                fugiat
+                similique velit facere nesciunt unde sapiente consequatur id soluta quibusdam praesentium cupiditate
+                dicta
+                quidem facilis adipisci?</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam in nobis, quas aspernatur doloremque
+                dolorum enim fugit, numquam quia assumenda asperiores sed, laboriosam fugiat repudiandae laborum
+                debitis.
+                Distinctio, aperiam eos!</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora blanditiis molestiae possimus obcaecati
+                deserunt, unde nihil pariatur praesentium asperiores doloribus vel et dolorum ab doloremque eius
+                similique
+                incidunt nisi quos.</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium nulla optio dolores, laudantium
+                fugiat
+                similique velit facere nesciunt unde sapiente consequatur id soluta quibusdam praesentium cupiditate
+                dicta
+                quidem facilis adipisci?</p>
+        </content>
+    </div>
+</main>
+
+@include('footer')
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('main');
 });
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::get('/register', function () {
+    return view('register');
+});
